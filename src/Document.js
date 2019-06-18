3bee2808eae16e9f9ea53b86cda2b9525e3a8c43
VrakasLabs <tim.vrakas@gmail.com>
2019-06-17 22:00:54 -0500

    Better Numbering

--- src/Document.js
+++ src/Document.js
@@ -24,7 +24,7 @@ class Document extends React.Component {
     }
 
     render() {
-        return <div dangerouslySetInnerHTML={this.state.data} />
+        return <div class="doc" dangerouslySetInnerHTML={this.state.data} />
     }
 
 }
